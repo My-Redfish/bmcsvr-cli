41d58b63c5355f130c30ec3bfdf7d218852ebd09
mark <marktsai0316@gmail.com>
2026-07-22 17:37:10 +0800

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -3,3 +3,16 @@ pwd
 ls -la
 TMP="${PWD}"
 echo "$TMP"
+
+
+sudo mkdir -p /usr/local/lib/bmcsvr
+
+sudo cp -a "$TMP/bmcsvr/." /usr/local/lib/bmcsvr/
+
+sudo ln -sf \
+    /usr/local/lib/bmcsvr/bmcsvr-cli.sh \
+    /usr/local/bin/bmcsvr
+
+rm -rf "$TMP"
+
+echo "Installed."
